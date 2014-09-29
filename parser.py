a91e8782484f06b8d97dd92e8983ff0fdea8ae21
Zoltan Borsos <deadnervster@gmail.com>
2014-09-29 11:48:38 +0200

    first init commit

---
+++ parser.py
@@ -0,0 +1,21 @@
+import configparser
+import csv
+
+Config = configparser.ConfigParser()
+Config.read("config.ini")
+
+def ConfigSectionMap(section):
+    dict1 = {}
+    options = Config.options(section)
+    for option in options:
+        try:
+            dict1[option] = Config.get(section, option)
+            if dict1[option] == -1:
+                DebugPrint("skip: %s" % option)
+        except:
+            print("exception on %s!" % option)
+            dict1[option] = None
+    return dict1
+
+url = ConfigSectionMap("ConfigRoutes")['url']
+print(url)
